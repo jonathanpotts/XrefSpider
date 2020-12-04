02ec9a2189cc9f94214db49a1bd14f426659bf54
Jonathan Potts <jonathanpotts@outlook.com>
2020-12-04 15:19:57 -0800

    Initial project commit

---
+++ src/XrefService/Controllers/XrefController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XrefService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class XrefController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Query([FromQuery] string uid)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/XrefService/Models/Xref.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace XrefService.Models
+{
+    /// <summary>
+    /// Xref (cross-reference) data.
+    /// </summary>
+    public class Xref
+    {
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
+        [JsonPropertyName("uid")]
+        public string UniqueId { get; set; }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Full name.
+        /// </summary>
+        [JsonPropertyName("fullName")]
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Hypertext reference (URL of reference page).
+        /// </summary>
+        [JsonPropertyName("href")]
+        public string HypertextReference { get; set; }
+
+        /// <summary>
+        /// Comment identifier.
+        /// </summary>
+        [JsonPropertyName("commentId")]
+        public string CommentId { get; set; }
+
+        /// <summary>
+        /// Monikers.
+        /// </summary>
+        [JsonPropertyName("monikers")]
+        public IList<string> Monikers { get; set; }
+
+        /// <summary>
+        /// Name with type.
+        /// </summary>
+        [JsonPropertyName("nameWithType")]
+        public string NameWithType { get; set; }
+
+        /// <summary>
+        /// Summary HTML.
+        /// </summary>
+        [JsonPropertyName("summary")]
+        public string SummaryHtml { get; set; }
+
+        /// <summary>
+        /// Tags.
+        /// </summary>
+        [JsonPropertyName("tags")]
+        public IList<string> Tags { get; set; }
+    }
+}
