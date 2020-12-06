3600763686b8ab60a86ca84340e8debf49d74ec8
Jonathan Potts <jonathanpotts@outlook.com>
2020-12-05 23:05:22 -0800

    Update docs.

--- src/XrefSpider/Models/Xref.cs
+++ src/XrefSpider/Models/Xref.cs
@@ -2,23 +2,44 @@
 
 namespace XrefSpider.Models
 {
+    /// <summary>
+    /// Xref (cross-reference) metadata.
+    /// </summary>
     public class Xref
     {
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
         [YamlMember(Alias = "uid")]
         public string UniqueId { get; set; }
 
+        /// <summary>
+        /// Name.
+        /// </summary>
         [YamlMember(Alias = "name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// URL to documentation page.
+        /// </summary>
         [YamlMember(Alias = "href")]
         public string Url { get; set; }
 
+        /// <summary>
+        /// Comment identifier.
+        /// </summary>
         [YamlMember(Alias = "commentId")]
         public string CommentId { get; set; }
 
+        /// <summary>
+        /// Full name.
+        /// </summary>
         [YamlMember(Alias = "fullName")]
         public string FullName { get; set; }
 
+        /// <summary>
+        /// Name with type.
+        /// </summary>
         [YamlMember(Alias = "nameWithType")]
         public string NameWithType { get; set; }
     }
